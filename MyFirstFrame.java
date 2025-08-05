7099bde3e4ac2e5f8d4985a831b65b0bf3a2f67f
Sdu23 <160658189+Sdu23@users.noreply.github.com>
2025-08-06 00:57:51 +0200

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,24 @@
+
+package za.ac.tut.ui;
+
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class MyFirstFrame extends JFrame{
+
+    public MyFirstFrame(){
+        setTitle("My First GUI");
+        setSize(500,550);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+   
+    
+    
+    
+}
